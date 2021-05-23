835e29fe3fc1ecc938cfc5c468eb08d0f4d7bf7c
franciscomcsousa <francisco.m.c.sousa@gmail.com>
2021-05-23 20:46:23 +0100

    added bakeoff-#2

---
+++ bakeoff-#2/1stIteration/sketch.js
@@ -0,0 +1,245 @@
+// Bakeoff #2 - Seleção de Alvos e Fatores Humanos
+// IPM 2020-21, Semestre 2
+// Entrega: até dia 7 de Maio às 23h59 através do Fenix
+// Bake-off: durante os laboratórios da semana de 3 de Maio
+
+// p5.js reference: https://p5js.org/reference/
+
+// Database (CHANGE THESE!)
+const GROUP_NUMBER   = 8;      // Add your group number here as an integer (e.g., 2, 3)
+const BAKE_OFF_DAY   = false;  // Set to 'true' before sharing during the simulation and bake-off days
+
+// Target and grid properties (DO NOT CHANGE!)
+let PPI, PPCM;
+let TARGET_SIZE;
+let TARGET_PADDING, MARGIN, LEFT_PADDING, TOP_PADDING;
+let continue_button;
+
+// Metrics
+let testStartTime, testEndTime;// time between the start and end of one attempt (48 trials)
+let hits 			 = 0;      // number of successful selections
+let misses 			 = 0;      // number of missed selections (used to calculate accuracy)
+let database;                  // Firebase DB  
+
+// Study control parameters
+let draw_targets     = false;  // used to control what to show in draw()
+let trials 			 = [];     // contains the order of targets that activate in the test
+let current_trial    = 0;      // the current trial number (indexes into trials array above)
+let attempt          = 0;      // users complete each test twice to account for practice (attemps 0 and 1)
+let fitts_IDs        = [];     // add the Fitts ID for each selection here (-1 when there is a miss)
+
+// Target class (position and width)
+class Target
+{
+  constructor(x, y, w)
+  {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+  }
+}
+
+// Runs once at the start
+function setup()
+{
+  createCanvas(700, 500);    // window size in px before we go into fullScreen()
+  frameRate(60);             // frame rate (DO NOT CHANGE!)
+  
+  randomizeTrials();         // randomize the trial order at the start of execution
+  
+  textFont("Arial", 18);     // font size for the majority of the text
+  drawUserIDScreen();        // draws the user input screen (student number and display size)
+}
+
+// Runs every frame and redraws the screen
+function draw()
+{
+  if (draw_targets)
+  {
+    // The user is interacting with the 4x4 target grid
+    background(color(0,0,0));        // sets background to black
+    
+    // Print trial count at the top left-corner of the canvas
+    fill(color(255,255,255));
+    textAlign(LEFT);
+    text("Trial " + (current_trial + 1) + " of " + trials.length, 50, 20);
+    
+    // Draw all 16 targets
+	for (var i = 0; i < 16; i++) drawTarget(i);
+  }
+}
+
+// Print and save results at the end of 48 trials
+function printAndSavePerformance()
+{
+  // DO NOT CHANGE THESE! 
+  let accuracy			= parseFloat(hits * 100) / parseFloat(hits + misses);
+  let test_time         = (testEndTime - testStartTime) / 1000;
+  let time_per_target   = nf((test_time) / parseFloat(hits + misses), 0, 3);
+  let penalty           = constrain((((parseFloat(95) - (parseFloat(hits * 100) / parseFloat(hits + misses))) * 0.2)), 0, 100);
+  let target_w_penalty	= nf(((test_time) / parseFloat(hits + misses) + penalty), 0, 3);
+  let timestamp         = day() + "/" + month() + "/" + year() + "  " + hour() + ":" + minute() + ":" + second();
+  
+  background(color(0,0,0));   // clears screen
+  fill(color(255,255,255));   // set text fill color to white
+  text(timestamp, 10, 20);    // display time on screen (top-left corner)
+  
+  textAlign(CENTER);
+  text("Attempt " + (attempt + 1) + " out of 2 completed!", width/2, 60); 
+  text("Hits: " + hits, width/2, 100);
+  text("Misses: " + misses, width/2, 120);
+  text("Accuracy: " + accuracy + "%", width/2, 140);
+  text("Total time taken: " + test_time + "s", width/2, 160);
+  text("Average time per target: " + time_per_target + "s", width/2, 180);
+  text("Average time for each target (+ penalty): " + target_w_penalty + "s", width/2, 220);
+  
+  // Print Fitts IDS (one per target, -1 if failed selection)
+  // 
+
+  // Saves results (DO NOT CHANGE!)
+  let attempt_data = 
+  {
+        project_from:       GROUP_NUMBER,
+        assessed_by:        student_ID,
+        test_completed_by:  timestamp,
+        attempt:            attempt,
+        hits:               hits,
+        misses:             misses,
+        accuracy:           accuracy,
+        attempt_duration:   test_time,
+        time_per_target:    time_per_target,
+        target_w_penalty:   target_w_penalty,
+        fitts_IDs:          fitts_IDs
+  }
+  
+  // Send data to DB (DO NOT CHANGE!)
+  if (BAKE_OFF_DAY)
+  {
+    // Access the Firebase DB
+    if (attempt === 0)
+    {
+      firebase.initializeApp(firebaseConfig);
+      database = firebase.database();
+    }
+    
+    // Add user performance results
+    let db_ref = database.ref('G' + GROUP_NUMBER);
+    db_ref.push(attempt_data);
+  }
+}
+
+// Mouse button was pressed - lets test to see if hit was in the correct target
+function mousePressed() 
+{
+  // Only look for mouse releases during the actual test
+  // (i.e., during target selections)
+  if (draw_targets)
+  {
+    // Get the location and size of the target the user should be trying to select
+    let target = getTargetBounds(trials[current_trial]);   
+    
+    // Check to see if the mouse cursor is inside the target bounds,
+    // increasing either the 'hits' or 'misses' counters
+    if (dist(target.x, target.y, mouseX, mouseY) < target.w/2)  hits++;                                                       
+    else misses++;
+    
+    current_trial++;                 // Move on to the next trial/target
+    
+    // Check if the user has completed all 48 trials
+    if (current_trial === trials.length)
+    {
+      testEndTime = millis();
+      draw_targets = false;          // Stop showing targets and the user performance results
+      printAndSavePerformance();     // Print the user's results on-screen and send these to the DB
+      attempt++;                      
+      
+      // If there's an attempt to go create a button to start this
+      if (attempt < 2)
+      {
+        continue_button = createButton('START 2ND ATTEMPT');
+        continue_button.mouseReleased(continueTest);
+        continue_button.position(width/2 - continue_button.size().width/2, height/2 - continue_button.size().height/2);
+      }
+    } 
+  }
+}
+
+// Draw target on-screen
+function drawTarget(i)
+{
+  // Get the location and size for target (i)
+  let target = getTargetBounds(i);             
+
+  // Check whether this target is the target the user should be trying to select
+  if (trials[current_trial] === i) 
+  { 
+    // Highlights the target the user should be trying to select
+    // with a white border
+    stroke(color(220,220,220));
+    strokeWeight(2);
+    
+    // Remember you are allowed to access targets (i-1) and (i+1)
+    // if this is the target the user should be trying to select
+    //
+  }
+  // Does not draw a border if this is not the target the user
+  // should be trying to select
+  else noStroke();          
+
+  // Draws the target
+  fill(color(155,155,155));                 
+  circle(target.x, target.y, target.w);
+}
+
+// Returns the location and size of a given target
+function getTargetBounds(i)
+{
+  var x = parseInt(LEFT_PADDING) + parseInt((i % 4) * (TARGET_SIZE + TARGET_PADDING) + MARGIN);
+  var y = parseInt(TOP_PADDING) + parseInt(Math.floor(i / 4) * (TARGET_SIZE + TARGET_PADDING) + MARGIN);
+
+  return new Target(x, y, TARGET_SIZE);
+}
+
+// Evoked after the user starts its second (and last) attempt
+function continueTest()
+{
+  // Re-randomize the trial order
+  shuffle(trials, true);
+  current_trial = 0;
+  print("trial order: " + trials);
+  
+  // Resets performance variables
+  hits = 0;
+  misses = 0;
+  fitts_IDs = [];
+  
+  continue_button.remove();
+  
+  // Shows the targets again
+  draw_targets = true;
+  testStartTime = millis();  
+}
+
+// Is invoked when the canvas is resized (e.g., when we go fullscreen)
+function windowResized() 
+{
+  resizeCanvas(windowWidth, windowHeight);
+    
+  let display    = new Display({ diagonal: display_size }, window.screen);
+
+  // DO NOT CHANGE THESE!
+  PPI            = display.ppi;                        // calculates pixels per inch
+  PPCM           = PPI / 2.54;                         // calculates pixels per cm
+  TARGET_SIZE    = 1.5 * PPCM;                         // sets the target size in cm, i.e, 1.5cm
+  TARGET_PADDING = 1.5 * PPCM;                         // sets the padding around the targets in cm
+  MARGIN         = 1.5 * PPCM;                         // sets the margin around the targets in cm
+
+  // Sets the margin of the grid of targets to the left of the canvas (DO NOT CHANGE!)
+  LEFT_PADDING   = width/2 - TARGET_SIZE - 1.5 * TARGET_PADDING - 1.5 * MARGIN;        
+  
+  // Sets the margin of the grid of targets to the top of the canvas (DO NOT CHANGE!)
+  TOP_PADDING    = height/2 - TARGET_SIZE - 1.5 * TARGET_PADDING - 1.5 * MARGIN;
+  
+  // Starts drawing targets immediately after we go fullscreen
+  draw_targets = true;
+}
---
+++ bakeoff-#2/1stIteration/support.js
@@ -0,0 +1,97 @@
+// Support variables & functions (DO NOT CHANGE!)
+
+let student_ID_form, display_size_form, start_button;                  // Initial input variables
+let student_ID, display_size;                                          // User input parameters
+
+// Prints the initial UI that prompts that ask for student ID and screen size
+function drawUserIDScreen()
+{ 
+  background(color(0,0,0));                                          // sets background to black
+  
+  // Text prompt
+  main_text = createDiv("Insert your student number and display size");
+  main_text.id('main_text');
+  main_text.position(10, 10);
+  
+  // Input forms:
+  // 1. Student ID
+  let student_ID_pos_y_offset = main_text.size().height + 40;         // y offset from previous item
+  
+  student_ID_form = createInput('');                                 // create input field
+  student_ID_form.position(200, student_ID_pos_y_offset);
+  
+  student_ID_label = createDiv("Student number (int)");              // create label
+  student_ID_label.id('input');
+  student_ID_label.position(10, student_ID_pos_y_offset);
+  
+  // 2. Display size
+  let display_size_pos_y_offset = student_ID_pos_y_offset + student_ID_form.size().height + 20;
+  
+  display_size_form = createInput('');                              // create input field
+  display_size_form.position(200, display_size_pos_y_offset);
+  
+  display_size_label = createDiv("Display size in inches");   // create label
+  display_size_label.id('input');
+  display_size_label.position(10, display_size_pos_y_offset);
+  
+  // 3. Start button
+  start_button = createButton('START');
+  start_button.mouseReleased(startTest);
+  start_button.position(width/2 - start_button.size().width/2, height/2 - start_button.size().height/2);
+}
+
+// Verifies if the student ID is a number, and within an acceptable range
+function validID()
+{
+  if(parseInt(student_ID_form.value()) < 200000 && parseInt(student_ID_form.value()) > 1000) return true
+  else 
+  {
+    alert("Please insert a valid student number (integer between 1000 and 200000)");
+	return false;
+  }
+}
+
+// Verifies if the display size is a number, and within an acceptable range
+function validSize()
+{
+  if (parseInt(display_size_form.value()) < 50 && parseInt(display_size_form.value()) > 10) return true
+  else
+  {
+    alert("Please insert a valid display size (between 10 and 50)");
+    return false;
+  }
+}
+
+// Starts the test (i.e., target selection task)
+function startTest()
+{
+  if (validID() && validSize())
+  {
+    // Saves student and display information
+    student_ID = parseInt(student_ID_form.value());
+    display_size = parseInt(display_size_form.value());
+
+    // Deletes UI elements
+    main_text.remove();
+    student_ID_form.remove();
+    student_ID_label.remove();
+    display_size_form.remove();
+    display_size_label.remove();
+    start_button.remove();  
+
+    // Goes fullscreen and starts test
+    fullscreen(!fullscreen());
+    testStartTime = millis();
+  }
+}
+
+// Randomize the order in the targets to be selected
+function randomizeTrials()
+{
+  for (var i = 0; i < 16; i++)        // 4 rows times 4 columns = 16 targets
+    for (var k = 0; k < 3; k++)       // each target will repeat 3 times (16 times 3 = 48 trials)
+      trials.push(i);
+  shuffle(trials, true);             // randomize the trial order
+
+  print("trial order: " + trials);   // prints trial order - for debug purposes
+}
